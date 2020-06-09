a54f2e00674f6657f7863824ace9bfddffef9dea
Huang Ying <ying.huang@intel.com>
2020-06-09 10:17:31 +0800

    memory tiering: check order in migrate_balanced_pgdat()
    When THP is promoted from the slow memory node to the fast memory
    node, migrate_balanced_pgdat() is called to check whether there's
    enough free space on the target node.  But originally, only the total
    number of the free pages is considered, but the order of the free
    pages isn't considered.  So it's possible that there's no free page
    with expected order, although the total number of the free pages is
    enough.  So that the page demotion for the fast memory node isn't
    initiated to make the high order pages available.
    
    To solve the above issue, the page order is considered in
    migrate_balanced_pgdat().
    
    TODO: test results?
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2132,8 +2132,7 @@ COMPAT_SYSCALL_DEFINE6(move_pages, pid_t, pid, compat_ulong_t, nr_pages,
  * Returns true if this is a safe migration target node for misplaced NUMA
  * pages. Currently it only checks the watermarks which crude
  */
-static bool migrate_balanced_pgdat(struct pglist_data *pgdat,
-				   unsigned long nr_migrate_pages)
+static bool migrate_balanced_pgdat(struct pglist_data *pgdat, int order)
 {
 	int z;
 
@@ -2143,10 +2142,8 @@ static bool migrate_balanced_pgdat(struct pglist_data *pgdat,
 		if (!populated_zone(zone))
 			continue;
 
-		/* Avoid waking kswapd by allocating pages_to_migrate pages. */
-		if (!zone_watermark_ok(zone, 0,
-				       high_wmark_pages(zone) +
-				       nr_migrate_pages,
+		/* Avoid waking kswapd by allocating pages to migrate. */
+		if (!zone_watermark_ok(zone, order, high_wmark_pages(zone),
 				       ZONE_MOVABLE, 0))
 			continue;
 		return true;
@@ -2171,12 +2168,12 @@ static struct page *alloc_misplaced_dst_page(struct page *page,
 
 static int numamigrate_isolate_page(pg_data_t *pgdat, struct page *page)
 {
-	int page_lru, nr = compound_nr(page), order = compound_order(page);
+	int page_lru, order = compound_order(page);
 
 	VM_BUG_ON_PAGE(order && !PageTransHuge(page), page);
 
 	/* Avoid migrating to a node that is nearly full */
-	if (!migrate_balanced_pgdat(pgdat, nr)) {
+	if (!migrate_balanced_pgdat(pgdat, order)) {
 		int z;
 
 		if (!(sysctl_numa_balancing_mode & NUMA_BALANCING_MEMORY_TIERING) ||
