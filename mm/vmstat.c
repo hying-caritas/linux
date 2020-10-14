df185312b060603eabfd99b59351380cd881ecb8
Huang Ying <ying.huang@intel.com>
2020-10-14 13:15:08 +0800

    memory tiering: add file pages promotion counter
    To distinguish the file pages promotion from the anonymous pages
    promotion.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1234,6 +1234,7 @@ const char * const vmstat_text[] = {
 	"pgpromote_success",
 	"pgpromote_candidate",
 	"promote_threshold",
+	"pgpromote_file",
 #endif
 
 	/* enum writeback_stat_item counters */
