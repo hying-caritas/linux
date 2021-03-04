27bdb5b6e6840fbcb0a85198c45aa2c5c4fd0c4f
Dave Hansen <dave.hansen@linux.intel.com>
2021-03-04 15:36:27 -0800

    mm/migrate: new zone_reclaim_mode to enable reclaim migration
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    Some method is obviously needed to enable reclaim-based migration.
    
    Just like traditional autonuma, there will be some workloads that
    will benefit like workloads with more "static" configurations where
    hot pages stay hot and cold pages stay cold.  If pages come and go
    from the hot and cold sets, the benefits of this approach will be
    more limited.
    
    The benefits are truly workload-based and *not* hardware-based.
    We do not believe that there is a viable threshold where certain
    hardware configurations should have this mechanism enabled while
    others do not.
    
    To be conservative, earlier work defaulted to disable reclaim-
    based migration and did not include a mechanism to enable it.
    This proposes extending the existing "zone_reclaim_mode" (now
    now really node_reclaim_mode) as a method to enable it.
    
    We are open to any alternative that allows end users to enable
    this mechanism or disable it it workload harm is detected (just
    like traditional autonuma).
    
    Once this is enabled page demotion may move data to a NUMA node
    that does not fall into the cpuset of the allocating process.
    This could be construed to violate the guarantees of cpusets.
    However, since this is an opt-in mechanism, the assumption is
    that anyone enabling it is content to relax the guarantees.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Yang Shi <yang.shi@linux.alibaba.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: osalvador <osalvador@suse.de>
    
    changes since 20200122:
     * Changelog material about relaxing cpuset constraints

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1073,6 +1073,9 @@ static bool migrate_demote_page_ok(struct page *page,
 	VM_BUG_ON_PAGE(PageHuge(page), page);
 	VM_BUG_ON_PAGE(PageLRU(page), page);
 
+	if (!(node_reclaim_mode & RECLAIM_MIGRATE))
+		return false;
+
 	/* It is pointless to do demotion in memcg reclaim */
 	if (cgroup_reclaim(sc))
 		return false;
@@ -1082,8 +1085,7 @@ static bool migrate_demote_page_ok(struct page *page,
 	if (PageTransHuge(page) && !thp_migration_supported())
 		return false;
 
-	// FIXME: actually enable this later in the series
-	return false;
+	return true;
 }
 
 /* Check if a page is dirty or under writeback */
